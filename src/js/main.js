262ad5fe9631f9983e41c091eab89fe35812d1fd
Bae hyowon <h.y.bae@runtime.co.kr>
2023-11-07 17:06:41 +0900

    프리티어 ignore 추가, css 사이공간 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,117 +1,116 @@
-(function($){
-
+(function ($) {
     const Common = {
-        init(){
+        init() {
             this.common();
         },
-        common(){
-            $('.all-colabds').click(function(){
-                $('.element').each(function() {
-                    $(this).find('h2').children().eq(0).html('add_circle_outlined')
-                    $(this).addClass('m0');
+        common() {
+            $(".all-colabds").click(function () {
+                $(".element").each(function () {
+                    $(this).find("h2").children().eq(0).html("add_circle_outlined");
+                    $(this).addClass("m0");
                 });
-                $('.elements').each(function(){
-                    $(this).addClass('dn');
-                })
-            });
-            
-            $('.all-epd').click(function(){
-                $('.element').each(function() {
-                    $(this).find('h2').children().eq(0).html('do_not_disturb_on')
-                    $(this).removeClass('m0');
+                $(".elements").each(function () {
+                    $(this).addClass("dn");
                 });
-                $('.elements').each(function(){
-                    $(this).removeClass('dn');
-                })
             });
-            
-            $('.element').on('click', 'h2 span', function() {
-                let ele = $(this).closest('.element').find('h2').children().eq(1);
-                let eles = $(this).closest('.element').find('.elements')
-            
-                ele.toggleClass('on');
-            
-                if (ele.hasClass('on')) {
-                    ele.siblings('i').html('add_circle_outlined')
-                    ele.parents('.element').addClass('m0');
-                    eles.addClass('dn');
+
+            $(".all-epd").click(function () {
+                $(".element").each(function () {
+                    $(this).find("h2").children().eq(0).html("do_not_disturb_on");
+                    $(this).removeClass("m0");
+                });
+                $(".elements").each(function () {
+                    $(this).removeClass("dn");
+                });
+            });
+
+            $(".element").on("click", "h2 span", function () {
+                let ele = $(this).closest(".element").find("h2").children().eq(1);
+                let eles = $(this).closest(".element").find(".elements");
+
+                ele.toggleClass("on");
+
+                if (ele.hasClass("on")) {
+                    ele.siblings("i").html("add_circle_outlined");
+                    ele.parents(".element").addClass("m0");
+                    eles.addClass("dn");
                 } else {
-                    ele.siblings('i').html('do_not_disturb_on')
-                    ele.parents('.element').removeClass('m0');
-                    eles.removeClass('dn');
+                    ele.siblings("i").html("do_not_disturb_on");
+                    ele.parents(".element").removeClass("m0");
+                    eles.removeClass("dn");
                 }
             });
 
-            $('.gp_left').on('click', function(e){
+            $(".gp_left").on("click", function (e) {
                 let target = e.target;
-                if(target.tagName === "A"){
-                    $(target).addClass('active');
+                if (target.tagName === "A") {
+                    $(target).addClass("active");
 
-                    let links = $('.gp_left A'); 
-                    for( let link of links ) {
-                        if(link !== target) { 
-                            $(link).removeClass('active');
+                    let links = $(".gp_left A");
+                    for (let link of links) {
+                        if (link !== target) {
+                            $(link).removeClass("active");
                         }
                     }
-                } 
+                }
             });
 
             // // textarea 오토사이징
-            $(document).on('keyup','textarea.autosize',function() {
-                $(this).css('height', 'auto' );
+            $(document).on("keyup", "textarea.autosize", function () {
+                $(this).css("height", "auto");
                 $(this).height(this.scrollHeight);
             });
 
             // input 내용 지우기
-            $('.input .del').on('click', function(){
-                $(this).siblings('input').val('');
-            })
+            $(".input .del").on("click", function () {
+                $(this).siblings("input").val("");
+            });
 
-            // input datepicker 
-            $('input.datepicker:not(.except)').datepicker({
+            // input datepicker
+            $("input.datepicker:not(.except)").datepicker({
                 showOn: "button",
-				changeYear:true ,
-				changeMonth:true,
-				buttonText: "달력",
-				showMonthAfterYear: true,
-				dateFormat:"yy-mm-dd",
-				yearRange: 'c-50:c+50',
+                changeYear: true,
+                changeMonth: true,
+                buttonText: "달력",
+                showMonthAfterYear: true,
+                dateFormat: "yy-mm-dd",
+                yearRange: "c-50:c+50",
                 // showOtherMonths : true,
                 // selectOtherMonths : true,
-				dayNamesMin: [ "일", "월", "화", "수", "목", "금", "토" ],
-				monthNamesShort: [ "1","2","3","4","5","6","7","8","9","10","11","12"]
+                dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"],
+                monthNamesShort: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
             });
 
             // 디데이 선택
-            function setupDatepickers(startDateId, endDateId){
+            function setupDatepickers(startDateId, endDateId) {
                 let startDatePicker = $("#" + startDateId);
-                let endDatePicker = $("#" + endDateId); 
-                let startDateVal =  startDatePicker.val();
-                let endDateVal =  endDatePicker.val();
+                let endDatePicker = $("#" + endDateId);
+                let startDateVal = startDatePicker.val();
+                let endDateVal = endDatePicker.val();
 
-                startDatePicker.datepicker( "setDate", new Date() )
-                startDatePicker.datepicker("option", "maxDate", endDateVal )
-                startDatePicker.datepicker("option", "onClose", function( selectedDate ){
-                    endDatePicker.datepicker( "option", "minDate", selectedDate );
-                })
+                startDatePicker.datepicker("setDate", new Date());
+                startDatePicker.datepicker("option", "maxDate", endDateVal);
+                startDatePicker.datepicker("option", "onClose", function (selectedDate) {
+                    endDatePicker.datepicker("option", "minDate", selectedDate);
+                });
 
-                endDatePicker.datepicker("option", "minDate", startDateVal )
-                endDatePicker.datepicker("option", "onClose", function( selectedDate){
-                    startDatePicker.datepicker( "option", "maxDate", selectedDate );
-                })
+                endDatePicker.datepicker("option", "minDate", startDateVal);
+                endDatePicker.datepicker("option", "onClose", function (selectedDate) {
+                    startDatePicker.datepicker("option", "maxDate", selectedDate);
+                });
             }
 
-            function setupMultipleDatepickersClass(startId, endId){
-                $(startId).each(function( idx , ele ) {
-                    let startDateId = $(ele).attr('id'); 
-                    let endDateId = $(ele).closest('.input').nextAll('.input').find(endId).attr('id');
+            function setupMultipleDatepickersClass(startId, endId) {
+                $(startId).each(function (idx, ele) {
+                    let startDateId = $(ele).attr("id");
+                    let endDateId = $(ele).closest(".input").nextAll(".input").find(endId).attr("id");
 
                     setupDatepickers(startDateId, endDateId);
                 });
             }
             setupMultipleDatepickersClass(".startDate", ".endDate");
-        }
-    }
+        },
+    };
 
     Common.init();
 })(jQuery);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // npm start 기본 세팅
 
 module.exports = {
   entry: './src/index.js',
@@ -21,7 +21,7 @@ module.exports = {
     })
   ],
   devServer: {
-    static: path.join(__dirname, 'src'),
+    static: path.join(__dirname, 'src'), // static 으로 설정해야함.
     port: 8080,
   },
 }
